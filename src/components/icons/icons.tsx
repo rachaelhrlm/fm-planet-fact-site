1d02cbb109ea024e68193f1f7a1613220b154f62
Rachael Morris <rachael.morris@sky.uk>
2022-03-26 08:44:17 +0000

    add icons

---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,4 @@
+import { ReactComponent as Chevron } from '../images/icon-chevron.svg';
+import { ReactComponent as Hamburger } from '../images/icon-hamburger.svg';
+
+export { Chevron, Hamburger };
