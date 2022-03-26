5efaa1533e7cc400ea2b6a84a1161ba455abd6c1
Rachael Morris <rachael.morris@sky.uk>
2022-03-26 09:27:43 +0000

    add menu

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames';
+import React, { FunctionComponent } from 'react';
+import { Planet } from '../../type';
+import { PlanetUtils } from '../../utils';
+import { MenuItem } from '../menuItem';
+
+export interface MenuProps {
+    isVisible: boolean;
+    onClick: (planet: Planet) => void;
+}
+
+export const Menu: FunctionComponent<MenuProps> = ({ isVisible, onClick }) => {
+    return (
+        <nav className={classNames('h-screen w-screen bg-black-500 justify-center pt-24 md:hidden px-6', { hidden: !isVisible })}>
+            {PlanetUtils.options.map((planet) => (
+                <MenuItem key={planet} onClick={() => onClick(planet)} planet={planet} />
+            ))}
+        </nav>
+    );
+};
