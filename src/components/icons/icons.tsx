59b92cc8a310139d9bfad9efb9a6bcf863454a30
Rachael Morris <rachael.morris@sky.uk>
2022-03-26 09:23:52 +0000

    add menu item

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as Chevron } from '../images/icon-chevron.svg';
-import { ReactComponent as Hamburger } from '../images/icon-hamburger.svg';
+import { ReactComponent as Chevron } from '../../images/icon-chevron.svg';
+import { ReactComponent as Hamburger } from '../../images/icon-hamburger.svg';
 
 export { Chevron, Hamburger };
---
+++ src/components/menuItem/menuItem.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react';
+import { Chevron } from '..';
+import { Planet } from '../../type';
+import { PlanetUtils } from '../../utils';
+
+export interface MenuItemProps {
+    onClick: () => void;
+    planet: Planet;
+}
+
+export const MenuItem: FunctionComponent<MenuItemProps> = ({ onClick, planet }) => {
+    return (
+        <button
+            onClick={onClick}
+            className="font-sans font-bold text-white uppercase border-b border-white/10 py-5 px-0 flex justify-between place-items-center w-full">
+            <h2 className="flex">
+                <div className={`h-5 w-5 ${PlanetUtils.colors[planet].nav} rounded-full mr-4`} /> {planet}
+            </h2>
+            <Chevron className="flex justify-self-end" />
+        </button>
+    );
+};
