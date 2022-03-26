8232c79a56063d2774bd9d2cbdc0eeb23fecead9
Rachael Morris <rachael.morris@sky.uk>
2022-03-26 09:28:25 +0000

    add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@ module.exports = {
     content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
+            backgroundImage: {
+                'starry-pattern': "url('../assets/background-stars.svg')",
+            },
             colors: {
                 black: {
                     500: '#070724',
                 },
                 earth: {
-                    500: '#D14C32',
+                    500: '#6F2ED6',
+                    600: '#545BFE',
                 },
                 grey: {
                     500: '#838391',
@@ -15,25 +19,35 @@ module.exports = {
                 },
                 jupiter: {
                     500: '#CD5120',
+                    600: '#ECAD7A',
                 },
                 mars: {
                     500: '#D83A34',
+                    600: '#FF6A45',
                 },
                 mercury: {
                     500: '#419EBB',
+                    600: '#DEF4FC',
                 },
                 neptune: {
                     500: '#2D68F0',
+                    600: '#497EFA',
+                },
+                saturn: {
+                    500: '#FCCB6B',
+                    600: '#FCCB6B',
                 },
                 uranus: {
                     500: '#1EC2A4',
+                    600: '#65F0D5',
                 },
                 venus: {
                     500: '#EDA249',
+                    600: '#F7CC7F',
                 },
             },
             fontFamily: {
-                heading: ['Antonio', 'sans-serif'],
+                title: ['Antonio', 'sans-serif'],
                 sans: ['Spartan', 'sans-serif'],
             },
         },
