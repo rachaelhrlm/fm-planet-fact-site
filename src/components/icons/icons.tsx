57495517e4062f8571a9bd21b2b218cf08207580
Rachael Morris <rachael.morris@sky.uk>
2022-03-27 16:44:17 +0100

    add source icon

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Chevron } from '../../images/icon-chevron.svg';
 import { ReactComponent as Hamburger } from '../../images/icon-hamburger.svg';
+import { ReactComponent as Source } from '../../images/icon-source.svg';
 
-export { Chevron, Hamburger };
+export { Chevron, Hamburger, Source };
