953289712025ce093c892e39ef0f13dbaec085b3
Rachael Morris <rachael.morris@sky.uk>
2022-03-27 18:43:44 +0100

    add tablet styling to statistics

--- src/components/statistics/statistics.tsx
+++ src/components/statistics/statistics.tsx
@@ -8,7 +8,7 @@ export interface StatisticsProps {
 
 export const Statistics: FunctionComponent<StatisticsProps> = ({ planetData }) => {
     return (
-        <section className="grid grid-cols-1 gap-2 pt-3 p-5 text-center">
+        <section className="grid grid-cols-1 gap-2 pt-3 p-5 md:grid-cols-4">
             <StatItem title="Rotation Time" value={planetData?.rotation} />
             <StatItem title="Revolution Time" value={planetData?.revolution} />
             <StatItem title="Radius" value={planetData?.radius} />
