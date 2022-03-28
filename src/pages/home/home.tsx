0edf594b0a9b639c8ff64e9f930af519a217df1d
Rachael Morris <rachael.morris@sky.uk>
2022-03-28 21:15:40 +0100

    update home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,31 +1,70 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Hamburger, Menu, NavBar } from '../../components';
-import { Planet } from '../../type';
+import { DetailImage, Navigation, SourceLink, Statistics, Title, ViewInfo, ViewSelect } from '../../components';
+import { Planet, PlanetData, View } from '../../type';
+import { PlanetUtils } from '../../utils';
 
 export const Home = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
-    const navigate = useNavigate();
+    const [selectedPlanet, setSelectedPlanet] = useState<Planet>(Planet.MERCURY);
+    const [planetData, setPlanetData] = useState<PlanetData>();
+    const [selectedView, setSelectedView] = useState<View>(View.OVERVIEW);
 
     const { planetId } = useParams();
 
+    useEffect(() => {
+        if (planetId && PlanetUtils.options.includes(planetId as Planet)) {
+            setSelectedPlanet(planetId as Planet);
+        }
+
+        return () => {
+            setSelectedPlanet(Planet.MERCURY);
+        };
+    }, [planetId]);
+
+    useEffect(() => {
+        PlanetUtils.getData(selectedPlanet).then((data) => {
+            setPlanetData(data);
+        });
+    }, [selectedPlanet]);
+
+    const navigate = useNavigate();
+
     return (
-        <div className="bg-black-500 w-screen h-screen bg-starry-pattern">
-            <header className="w-full border-b-2 border-white/10 p-5 flex justify-between place-items-center fixed bg-black-500 bg-starry-pattern md:pt-10 md:block md:text-center xl:pt-5 xl:flex xl:relative">
-                <h1 className="uppercase text-white text-title font-title text-3xl xl:flex-1 xl:text-left">The Planets</h1>
-                <button className="md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    <Hamburger />
-                </button>
-                <NavBar onClick={(planet: Planet) => navigate(`/${planet}`)} />
-            </header>
-            <Menu
-                isVisible={isMenuOpen}
-                onClick={(planet: Planet) => {
-                    setIsMenuOpen(false);
-                    navigate(`/${planet}`);
-                }}
+        <div className="bg-black-500 w-full min-h-screen bg-starry-pattern">
+            <Navigation
+                onNavigate={(path: string) => navigate(path)}
+                onViewSelect={(view: View) => setSelectedView(view)}
+                planet={selectedPlanet}
+                view={selectedView}
             />
+
+            <main className="pt-36 grid grid-cols-1 justify-center md:pt-0 xl:justify-items-center">
+                <div className="sm:p-5 xl:w-[1240px] 2xl:w-[1300px]">
+                    <div className="xl:flex xl:place-items-center">
+                        <div className="w-full h-width flex justify-center place-items-center p-6 relative sm:h-[20rem] md:h-[30rem] xl:h-[35rem] 2xl:h-[40rem]">
+                            <img
+                                style={{ width: `auto`, height: `${PlanetUtils.getRadius(planetData)}%`, objectFit: 'contain' }}
+                                src={`${process.env.PUBLIC_URL}${PlanetUtils.getImage(selectedView, planetData)}`}
+                                alt={`${selectedPlanet} ${selectedView}`}
+                            />
+                            <DetailImage isVisible={selectedView === View.SURFACE} planetData={planetData} view={selectedView} />
+                        </div>
+
+                        <div className="md:flex justify-between place-items-center xl:block xl:w-[50%]">
+                            <section className="grid grid-cols-1 gap-5 p-5 text-center place-content-between h-[19rem] md:h-[16rem] md:text-left md:w-1/2 xl:w-full xl:h-[18rem]">
+                                <div className="grid grid-cols-1 gap-[1rem] xl:gap-[2.5rem]">
+                                    <Title>{planetData?.name}</Title>
+                                    <ViewInfo>{PlanetUtils.getViewInfo(selectedView, planetData)?.content}</ViewInfo>
+                                </div>
+                                <SourceLink link={PlanetUtils.getViewInfo(selectedView, planetData)?.source} />
+                            </section>
+
+                            <ViewSelect onClick={(view: View) => setSelectedView(view)} planet={selectedPlanet} view={selectedView} />
+                        </div>
+                    </div>
+                    <Statistics planetData={planetData} />
+                </div>
+            </main>
         </div>
     );
 };
